7a3bda75348976359b715cbb6741f64acf368739
Mark <markosmk@me.com>
2021-12-05 13:01:39 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    MONGO_SV:
-      'mongodb+srv://makc:lkI2yZOEqMCgEtbb@apiecommerce.u4cjt.mongodb.net/mern-next?retryWrites=true&w=majority',
+    MONGO_SV: '',
     JWT_SECRET: '',
     CLOUDINARY_URL: '',
   },
