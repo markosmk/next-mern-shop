a33b4159ea7cf89aec6d6739b8ae537d1fff92ae
Markos <markosmk@me.com>
2021-12-05 09:59:47 -0300

    add - uri connect mongodb

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    MONGO_SV: '',
+    MONGO_SV:
+      'mongodb+srv://makc:lkI2yZOEqMCgEtbb@apiecommerce.u4cjt.mongodb.net/mern-next?retryWrites=true&w=majority',
     JWT_SECRET: '',
     CLOUDINARY_URL: '',
   },
-};
+}
