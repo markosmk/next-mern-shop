98213fb45df3e2d5c5f678d404e8047150e6a2bf
Markos <markosmk@me.com>
2021-12-05 21:04:35 -0300

    update - add try catch for errors

--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -4,17 +4,23 @@ import connectDb from '../../../utils/connectDb'
 async function handlePostRequest(req, res) {
   if (req.method === 'POST') {
     const { name, price, description, mediaUrl } = req.body
-    if (!name || !price || !description || !mediaUrl) {
-      return res.status(422).send('Product missing one or more fields')
-    }
-    const newData = await new Product({
-      name,
-      price,
-      description,
-      mediaUrl,
-    }).save()
-    if (newData) {
-      return res.status(201).json({ status: 'success', data: newData })
+
+    try {
+      if (!name || !price || !description || !mediaUrl) {
+        return res.status(422).send('Product missing one or more fields')
+      }
+      const newData = await new Product({
+        name,
+        price,
+        description,
+        mediaUrl,
+      }).save()
+      if (newData) {
+        return res.status(201).json({ status: 'success', data: newData })
+      }
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Server error creating product')
     }
   }
 
