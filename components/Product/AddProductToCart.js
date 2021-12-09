9a03e2a16abdd145d5145f8381aa23f05de1d797
Markos <markosmk@me.com>
2021-12-08 23:49:31 -0300

    add - function addToCart

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import { Input } from 'semantic-ui-react'
 
 function AddProductToCart() {
+  const [quantity, setQuantity] = useState(1)
+
   return (
     <>
       <Input
         type="number"
         min="1"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
         placeholder="Quantity"
         action={{ color: 'orange', content: 'Add to Cart', icon: 'plus cart' }}
       />
--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,31 @@
+import axios from 'axios'
+import { parseCookies } from 'nookies'
 import { Segment } from 'semantic-ui-react'
 import CartItemList from '../components/Cart/CartItemList'
 import CartSummary from '../components/Cart/CartSummary'
+import baseUrl from '../utils/baseUrl'
 
-function Cart() {
+function Cart({ products }) {
+  console.log(products)
   return (
     <Segment>
       <CartItemList />
       <CartSummary />
     </Segment>
   )
+}
+
+Cart.getInitialProps = async ({ ctx }) => {
+  const { token } = parseCookies(ctx)
+
+  if (!token) {
+    return { products: [] }
+  }
+  const url = `${baseUrl}/api/cart`
+  const options = { headers: { Authorization: token } }
+  const response = await axios.get(url, options)
+  console.log('data', response.data)
+  return { products: response.data }
 }
 
 export default Cart
